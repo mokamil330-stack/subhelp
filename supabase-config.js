4390d788b09736ac500a28e39048a01cf4ba3c00
mokamil330-stack <mokamil330@gmail.com>
2026-09-02 08:47:56 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,1 +1,9 @@
 
+const SUPABASE_URL = "https://ojvzqsxfdaknxdawffhu.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY=sb_publishable_O1hvQrgIyHW5XYgaWcygmQ_JaX9VI-T";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
